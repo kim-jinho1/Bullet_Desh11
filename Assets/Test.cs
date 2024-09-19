f3dc274c822a5fab29cdbe34e6349e190f814a61
김진호 <russiakim2008@gmail.com>
2024-09-19 19:12:36 +0900

    Add
    Add

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("aa");
+        Debug.Log("Niga");
     }
 
     // Update is called once per frame
